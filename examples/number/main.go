6524181ad6603a9086e7d0db65faaa7e812c47ac
yacut <roman.rogozhnikov@gmail.com>
2020-10-19 11:06:46 +0200

    add number example

---
+++ examples/number/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/iLert/ilert-go"
+)
+
+func main() {
+	client := ilert.NewClient()
+	result, err := client.GetNumbers(&ilert.GetNumbersInput{})
+	if err != nil {
+		log.Println(result)
+		log.Fatalln("ERROR:", err)
+	}
+	log.Println(fmt.Sprintf("Found %d numbers\n\n ", len(result.Numbers)))
+	for _, number := range result.Numbers {
+		log.Println(fmt.Sprintf("%+v\n", *number))
+	}
+}
