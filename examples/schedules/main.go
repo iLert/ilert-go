ccc77fbb47e738aae392543defcf8913b1428dce
Roman Frey <roman.frey2020@gmail.com>
2023-01-11 11:56:59 +0100

    adjust schedule sample

--- examples/schedules/main.go
+++ examples/schedules/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"log"
 
 	"github.com/iLert/ilert-go/v2"
@@ -10,13 +9,38 @@ import (
 func main() {
 	var apiToken = "your API token"
 	client := ilert.NewClient(ilert.WithAPIToken(apiToken))
-	result, err := client.GetSchedules(&ilert.GetSchedulesInput{})
+
+	maxResultsProPage := 10
+
+	// Fetch first page
+	resultFirstPage0, err := client.GetSchedules(&ilert.GetSchedulesInput{
+		StartIndex: ilert.Int(0),
+		MaxResults: ilert.Int(maxResultsProPage),
+	})
 	if err != nil {
-		log.Println(result)
+		log.Println(resultFirstPage0)
 		log.Fatalln("ERROR:", err)
+		return
 	}
-	log.Println(fmt.Sprintf("Found %d schedules\n\n ", len(result.Schedules)))
-	for _, schedule := range result.Schedules {
-		log.Println(fmt.Sprintf("%+v\n", *schedule))
+	log.Printf("Found %d schedules on first page\n\n ", len(resultFirstPage0.Schedules))
+	for _, schedule := range resultFirstPage0.Schedules {
+		log.Printf("%+v\n", *schedule)
+	}
+
+	if len(resultFirstPage0.Schedules) == 10 {
+		// Fetch second page
+		resultSecondPage, err := client.GetSchedules(&ilert.GetSchedulesInput{
+			StartIndex: ilert.Int(1),
+			MaxResults: ilert.Int(maxResultsProPage),
+		})
+		if err != nil {
+			log.Println(resultSecondPage)
+			log.Fatalln("ERROR:", err)
+			return
+		}
+		log.Printf("Found %d schedules on second page\n\n ", len(resultSecondPage.Schedules))
+		for _, schedule := range resultSecondPage.Schedules {
+			log.Printf("%+v\n", *schedule)
+		}
 	}
 }
