7c9876a321e99a0bc82b1f1149dadb210fc2348c
Marko Simon <marko@ilert.com>
2024-01-12 21:54:05 +0100

    fix typo

--- alert_action.go
+++ alert_action.go
@@ -222,7 +222,7 @@ type AlertActionParamsAutomationRule struct {
 
 // AlertActionParamsTelegram definition
 type AlertActionParamsTelegram struct {
-	ChannelId string `json:"channelId,omitempty"`
+	ChannelID string `json:"channelId,omitempty"`
 }
 
 // AlertActionResult definition
